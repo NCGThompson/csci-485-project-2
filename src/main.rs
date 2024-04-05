92c02a122f6e3d9e44a4ad9219080cbe8fd9eddf
Jerry A Rogers <117837770+Windell2468@users.noreply.github.com>
2024-04-05 14:25:16 -0400

    format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::env;
 
 fn main() {
-    let args: Vec<String> =  env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     let mut file_path: String = args[1].to_owned();
 
     println!("{}", file_path);
-
 }
-
